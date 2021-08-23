4b61c178576158d301c9f0cc6bf24c86fc20eb6a
Abel Mare Cester <abelmc2001@gmail.com>
2021-08-23 18:50:22 +0200

    Google Sign added

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -6,7 +6,7 @@ const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 const googleVerify = async( id_token ) => {
   const ticket = await client.verifyIdToken({
       idToken: id_token,
-      audience: process.env.GOOGLE_CLIENT_I,  // Specify the CLIENT_ID of the app that accesses the backend
+      audience: process.env.GOOGLE_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
       // Or, if multiple clients access the backend:
       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
   });
